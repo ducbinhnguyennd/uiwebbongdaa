af05583c2d150430c5317a74c5bd841f2d01634a
trafdual <traz08102003@gmail.com>
2024-11-09 17:32:24 +0700

    f

--- src/Layout/LoginLayout/Logintong.js
+++ src/Layout/LoginLayout/Logintong.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Logintong.scss'
 // import { publicRoutes } from '../../router'
